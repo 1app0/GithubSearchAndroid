a5d9b0e5ca499de3efd2c034ddb8966afd5dd349
Alex Vasilianov <293173@via.dk>
2021-05-11 23:57:05 +0300

    Favorite tab view now shows the list of favorite users, and updates accordingly when a new favorite user is added deleted. Only searching for a favorite user remains.

--- app/src/main/java/com/example/githubsearchandroid/ui/favourite/FavoriteFragment.java
+++ app/src/main/java/com/example/githubsearchandroid/ui/favourite/FavoriteFragment.java
@@ -10,9 +10,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.githubsearchandroid.R;
 import com.example.githubsearchandroid.data.FavData.FavUser;
+import com.example.githubsearchandroid.ui.favourite.adapter.UserAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,6 +29,8 @@ public class FavoriteFragment extends Fragment {
     private FavoriteViewModel viewModel;
     private List<FavUser> favUserList;
     private DatabaseReference dbRef;
+    private RecyclerView recyclerView;
+    private UserAdapter adapter;
 
     private final ValueEventListener listener = new ValueEventListener() {
         @Override
@@ -33,7 +39,6 @@ public class FavoriteFragment extends Fragment {
 
             for (DataSnapshot snap: snapshot.getChildren()) {
                 FavUser favUser = snap.getValue(FavUser.class);
-                Log.i("favUserList", favUser.getBody());
                 favUserList.add(favUser);
             }
         }
@@ -52,6 +57,18 @@ public class FavoriteFragment extends Fragment {
         favUserList = new ArrayList<>();
         dbRef = viewModel.getDbRef();
 
+        recyclerView = root.findViewById(R.id.recycleView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.hasFixedSize();
+
+        FirebaseRecyclerOptions<FavUser> options = new FirebaseRecyclerOptions.Builder<FavUser>()
+                .setQuery(dbRef, FavUser.class)
+                .build();
+
+        adapter = new UserAdapter(options);
+        recyclerView.setAdapter(adapter);
+
         return root;
     }
 
@@ -59,11 +76,13 @@ public class FavoriteFragment extends Fragment {
     public void onResume() {
         super.onResume();
         dbRef.addValueEventListener(listener);
+        adapter.startListening();
     }
 
     @Override
     public void onPause() {
         super.onPause();
         dbRef.removeEventListener(listener);
+        adapter.stopListening();
     }
 }
---
+++ app/src/main/java/com/example/githubsearchandroid/ui/favourite/adapter/UserAdapter.java
@@ -0,0 +1,61 @@
+package com.example.githubsearchandroid.ui.favourite.adapter;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.githubsearchandroid.R;
+import com.example.githubsearchandroid.data.FavData.FavRepository;
+import com.example.githubsearchandroid.data.FavData.FavUser;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+import java.util.List;
+
+public class UserAdapter extends FirebaseRecyclerAdapter<FavUser, UserAdapter.ViewHolder> {
+    private List<FavUser> favUserList;
+    private FavRepository favRepo;
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public UserAdapter(@NonNull FirebaseRecyclerOptions<FavUser> options) {
+        super(options);
+    }
+
+    @NonNull
+    @Override
+    public UserAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View root = inflater.inflate(R.layout.favorite_list_item, parent, false);
+        return new ViewHolder(root);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull FavUser model) {
+        holder.favUsername.setText(model.getBody());
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView favUsername;
+        private Button favButton;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            favUsername = itemView.findViewById(R.id.favoriteName_textView);
+            favButton = itemView.findViewById(R.id.favoriteButton);
+
+            favButton.setOnClickListener(v -> Log.i("favUserSearch", String.valueOf(favUsername.getText())));
+        }
+    }
+}
